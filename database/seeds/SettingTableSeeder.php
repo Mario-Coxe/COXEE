7d697c1a689477a1a4f30be4364e13051ee71914
Mário Coxe <mario.coxe@ideiasdinamicas.com>
2024-03-05 13:06:03 +0100

    feat: remove logo e add text Logo in Header

--- database/seeds/SettingTableSeeder.php
+++ database/seeds/SettingTableSeeder.php
@@ -11,16 +11,20 @@ class SettingTableSeeder extends Seeder
      */
     public function run()
     {
-        $data=array(
-            'description'=>"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspiciatis unde sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspiciatis unde omnis iste natus error sit voluptatem Excepteu
+        $data = array(
+            'description' => "Coxee é uma plataforma de comércio eletrônico líder em Angola, oferecendo uma vasta seleção de produtos e serviços para atender às necessidades dos consumidores angolanos. Com uma interface amigável e intuitiva, Coxee torna a experiência de compra online fácil e conveniente para todos os usuários.
 
-                            sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspiciatis Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspi deserunt mollit anim id est laborum. sed ut perspi.",
-            'short_des'=>"Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.",
-            'photo'=>"image.jpg",
-            'logo'=>'logo.jpg',
-            'address'=>"115 Test Street, Test Country",
-            'email'=>"codeastro.com",
-            'phone'=>"1234567777",
+Navegue por uma ampla gama de categorias, desde eletrônicos de última geração até moda, produtos de beleza, artigos para o lar e muito mais. Cada produto é cuidadosamente apresentado com imagens nítidas e descrições detalhadas para garantir que os clientes façam escolhas informadas.
+
+A segurança é uma prioridade na Coxee. Oferecemos uma variedade de opções de pagamento seguras, incluindo cartões de crédito, débito e outros métodos de pagamento online populares em Angola. Além disso, nossa equipe está empenhada em garantir que suas compras sejam entregues de forma rápida e confiável.
+
+Desfrute de recursos adicionais, como avaliações de produtos, sistema de classificação de vendedores e suporte ao cliente dedicado para garantir uma experiência de compra excepcional em Coxee. Junte-se a nós e descubra uma nova maneira conveniente e confiável de fazer compras online em Angola. Coxee - onde suas necessidades encontram conveniência.",
+            'short_des' => "Coxee é uma plataforma de comércio eletrônico que oferece uma ampla variedade de produtos e serviços para consumidores em Angola. O site provavelmente inclui uma interface intuitiva que permite aos usuários navegar facilmente pelos produtos, categorias e ofertas especiais.",
+            'photo' => "image.jpg",
+            'logo' => 'logo.jpg',
+            'address' => "Angola, Luanda, Sapú 2",
+            'email' => "coxee.com",
+            'phone' => "945047525",
         );
         DB::table('settings')->insert($data);
     }
