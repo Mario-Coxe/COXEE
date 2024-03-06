33b793fe35ae4d69ccdf1f11cdb3d68ffb4ccf75
Mário Coxe <mario.coxe@ideiasdinamicas.com>
2024-03-06 15:53:03 +0100

    feat: update header

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -8,12 +8,19 @@
                     <div class="top-left">
                         <ul class="list-main">
                             @php
-                            $settings=DB::table('settings')->get();
+                            $settings = DB::table('settings')->get();
 
                             @endphp
-                            <li><i class="ti-headphone-alt"></i>@foreach($settings as $data) {{$data->phone}}
-                                @endforeach</li>
-                            <li><i class="ti-email"></i> @foreach($settings as $data) {{$data->email}} @endforeach</li>
+                            <li><i class="ti-headphone-alt"></i>
+                                @foreach ($settings as $data)
+                                {{ $data->phone }}
+                                @endforeach
+                            </li>
+                            <li><i class="ti-email"></i>
+                                @foreach ($settings as $data)
+                                {{ $data->email }}
+                                @endforeach
+                            </li>
                         </ul>
                     </div>
                     <!--/ End Top Left -->
@@ -24,20 +31,24 @@
                         <ul class="list-main">
                             {{-- <li><i class="ti-alarm-clock"></i> <a href="#">Daily deal</a></li> --}}
                             @auth
-                            @if(Auth::user()->role=='admin')
-                            <li><i class="fa fa-truck"></i> <a href="{{route('order.track')}}">Track Order</a></li>
+                            @if (Auth::user()->role == 'admin')
+                            <li><i class="fa fa-truck"></i> <a href="{{ route('order.track') }}">Acompanhar Pedido</a>
+                            </li>
 
-                            <li><i class="ti-user"></i> <a href="{{route('admin')}}" target="_blank">Dashboard</a></li>
+                            <li><i class="ti-user"></i> <a href="{{ route('admin') }}" target="_blank">Dashboard</a>
+                            </li>
                             @else
-                            <li><i class="fa fa-truck"></i> <a href="{{route('order.track')}}">Track Order</a></li>
+                            <li><i class="fa fa-truck"></i> <a href="{{ route('order.track') }}">Acompanhar Pedido</a>
+                            </li>
 
-                            <li><i class="ti-user"></i> <a href="{{route('user')}}" target="_blank">Dashboard</a></li>
+                            <li><i class="ti-user"></i> <a href="{{ route('user') }}" target="_blank">Dashboard</a>
+                            </li>
                             @endif
-                            <li><i class="ti-power-off"></i> <a href="{{route('user.logout')}}">Logout</a></li>
-
+                            <li><i class="ti-power-off"></i> <a href="{{ route('user.logout') }}">Terminar Sessão</a>
+                            </li>
                             @else
-                            <li><i class="fa fa-sign-in"></i><a href="{{route('login.form')}}">Login /</a> <a
-                                    href="{{route('register.form')}}">Register</a></li>
+                            <li><i class="fa fa-sign-in"></i><a href="{{ route('login.form') }}">Entrar /</a> <a
+                                    href="{{ route('register.form') }}">Registrar</a></li>
                             @endauth
                         </ul>
                     </div>
@@ -54,7 +65,7 @@
                     <!-- Logo -->
                     <div class="logo">
                         @php
-                        $settings=DB::table('settings')->get();
+                        $settings = DB::table('settings')->get();
                         @endphp
                         <h4>Logo</h4>
                     </div>
@@ -65,7 +76,7 @@
                         <!-- Search Form -->
                         <div class="search-top">
                             <form class="search-form">
-                                <input type="text" placeholder="Search here..." name="search">
+                                <input type="text" placeholder="Procure aqui..." name="search">
                                 <button value="search" type="submit"><i class="ti-search"></i></button>
                             </form>
                         </div>
@@ -78,14 +89,14 @@
                     <div class="search-bar-top">
                         <div class="search-bar">
                             <select>
-                                <option>All Category</option>
-                                @foreach(Helper::getAllCategory() as $cat)
-                                <option>{{$cat->title}}</option>
+                                <option>Todas</option>
+                                @foreach (Helper::getAllCategory() as $cat)
+                                <option>{{ $cat->title }}</option>
                                 @endforeach
                             </select>
-                            <form method="POST" action="{{route('product.search')}}">
+                            <form method="POST" action="{{ route('product.search') }}">
                                 @csrf
-                                <input name="search" placeholder="Search Products Here....." type="search">
+                                <input name="search" placeholder="Pesquise produtos aqui....." type="search">
                                 <button class="btnn" type="submit"><i class="ti-search"></i></button>
                             </form>
                         </div>
@@ -96,40 +107,41 @@
                         <!-- Search Form -->
                         <div class="sinlge-bar shopping">
                             @php
-                            $total_prod=0;
-                            $total_amount=0;
+                            $total_prod = 0;
+                            $total_amount = 0;
                             @endphp
-                            @if(session('wishlist'))
-                            @foreach(session('wishlist') as $wishlist_items)
+                            @if (session('wishlist'))
+                            @foreach (session('wishlist') as $wishlist_items)
                             @php
-                            $total_prod+=$wishlist_items['quantity'];
-                            $total_amount+=$wishlist_items['amount'];
+                            $total_prod += $wishlist_items['quantity'];
+                            $total_amount += $wishlist_items['amount'];
                             @endphp
                             @endforeach
                             @endif
-                            <a href="{{route('wishlist')}}" class="single-icon"><i class="fa fa-heart-o"></i> <span
-                                    class="total-count">{{Helper::wishlistCount()}}</span></a>
+                            <a href="{{ route('wishlist') }}" class="single-icon"><i class="fa fa-heart-o"></i> <span
+                                    class="total-count">{{ Helper::wishlistCount() }}</span></a>
                             <!-- Shopping Item -->
                             @auth
                             <div class="shopping-item">
                                 <div class="dropdown-cart-header">
-                                    <span>{{count(Helper::getAllProductFromWishlist())}} Items</span>
-                                    <a href="{{route('wishlist')}}">View Wishlist</a>
+                                    <span>{{ count(Helper::getAllProductFromWishlist()) }} Items</span>
+                                    <a href="{{ route('wishlist') }}">View Wishlist</a>
                                 </div>
                                 <ul class="shopping-list">
                                     {{-- {{Helper::getAllProductFromCart()}} --}}
-                                    @foreach(Helper::getAllProductFromWishlist() as $data)
+                                    @foreach (Helper::getAllProductFromWishlist() as $data)
                                     @php
-                                    $photo=explode(',',$data->product['photo']);
+                                    $photo = explode(',', $data->product['photo']);
                                     @endphp
                                     <li>
-                                        <a href="{{route('wishlist-delete',$data->id)}}" class="remove"
+                                        <a href="{{ route('wishlist-delete', $data->id) }}" class="remove"
                                             title="Remove this item"><i class="fa fa-remove"></i></a>
-                                        <a class="cart-img" href="#"><img src="{{$photo[0]}}" alt="{{$photo[0]}}"></a>
-                                        <h4><a href="{{route('product-detail',$data->product['slug'])}}"
-                                                target="_blank">{{$data->product['title']}}</a></h4>
-                                        <p class="quantity">{{$data->quantity}} x - <span
-                                                class="amount">${{number_format($data->price,2)}}</span></p>
+                                        <a class="cart-img" href="#"><img src="{{ $photo[0] }}"
+                                                alt="{{ $photo[0] }}"></a>
+                                        <h4><a href="{{ route('product-detail', $data->product['slug']) }}"
+                                                target="_blank">{{ $data->product['title'] }}</a></h4>
+                                        <p class="quantity">{{ $data->quantity }} x - <span
+                                                class="amount">${{ number_format($data->price, 2) }}</span></p>
                                     </li>
                                     @endforeach
                                 </ul>
@@ -137,9 +149,9 @@
                                     <div class="total">
                                         <span>Total</span>
                                         <span
-                                            class="total-amount">${{number_format(Helper::totalWishlistPrice(),2)}}</span>
+                                            class="total-amount">${{ number_format(Helper::totalWishlistPrice(), 2) }}</span>
                                     </div>
-                                    <a href="{{route('cart')}}" class="btn animate">Cart</a>
+                                    <a href="{{ route('cart') }}" class="btn animate">Cart</a>
                                 </div>
                             </div>
                             @endauth
@@ -150,38 +162,38 @@
                             aria-hidden="true"></i></a>
                     </div> --}}
                     <div class="sinlge-bar shopping">
-                        <a href="{{route('cart')}}" class="single-icon"><i class="ti-bag"></i> <span
-                                class="total-count">{{Helper::cartCount()}}</span></a>
+                        <a href="{{ route('cart') }}" class="single-icon"><i class="ti-bag"></i> <span
+                                class="total-count">{{ Helper::cartCount() }}</span></a>
                         <!-- Shopping Item -->
                         @auth
                         <div class="shopping-item">
                             <div class="dropdown-cart-header">
-                                <span>{{count(Helper::getAllProductFromCart())}} Items</span>
-                                <a href="{{route('cart')}}">View Cart</a>
+                                <span>{{ count(Helper::getAllProductFromCart()) }} Items</span>
+                                <a href="{{ route('cart') }}">View Cart</a>
                             </div>
                             <ul class="shopping-list">
                                 {{-- {{Helper::getAllProductFromCart()}} --}}
-                                @foreach(Helper::getAllProductFromCart() as $data)
+                                @foreach (Helper::getAllProductFromCart() as $data)
                                 @php
-                                $photo=explode(',',$data->product['photo']);
+                                $photo = explode(',', $data->product['photo']);
                                 @endphp
                                 <li>
-                                    <a href="{{route('cart-delete',$data->id)}}" class="remove"
+                                    <a href="{{ route('cart-delete', $data->id) }}" class="remove"
                                         title="Remove this item"><i class="fa fa-remove"></i></a>
-                                    <a class="cart-img" href="#"><img src="{{$photo[0]}}" alt="{{$photo[0]}}"></a>
-                                    <h4><a href="{{route('product-detail',$data->product['slug'])}}"
-                                            target="_blank">{{$data->product['title']}}</a></h4>
-                                    <p class="quantity">{{$data->quantity}} x - <span
-                                            class="amount">${{number_format($data->price,2)}}</span></p>
+                                    <a class="cart-img" href="#"><img src="{{ $photo[0] }}" alt="{{ $photo[0] }}"></a>
+                                    <h4><a href="{{ route('product-detail', $data->product['slug']) }}"
+                                            target="_blank">{{ $data->product['title'] }}</a></h4>
+                                    <p class="quantity">{{ $data->quantity }} x - <span
+                                            class="amount">${{ number_format($data->price, 2) }}</span></p>
                                 </li>
                                 @endforeach
                             </ul>
                             <div class="bottom">
                                 <div class="total">
                                     <span>Total</span>
-                                    <span class="total-amount">${{number_format(Helper::totalCartPrice(),2)}}</span>
+                                    <span class="total-amount">${{ number_format(Helper::totalCartPrice(), 2) }}</span>
                                 </div>
-                                <a href="{{route('checkout')}}" class="btn animate">Checkout</a>
+                                <a href="{{ route('checkout') }}" class="btn animate">Checkout</a>
                             </div>
                         </div>
                         @endauth
@@ -204,21 +216,21 @@
                                 <div class="navbar-collapse">
                                     <div class="nav-inner">
                                         <ul class="nav main-menu menu navbar-nav">
-                                            <li class="{{Request::path()=='home' ? 'active' : ''}}"><a
-                                                    href="{{route('home')}}">Home</a></li>
-                                            <li class="{{Request::path()=='about-us' ? 'active' : ''}}"><a
-                                                    href="{{route('about-us')}}">About Us</a></li>
+                                            <li class="{{ Request::path() == 'home' ? 'active' : '' }}"><a
+                                                    href="{{ route('home') }}">Home</a></li>
+                                            <!-- <li class="{{ Request::path() == 'about-us' ? 'active' : '' }}"><a
+                                                    href="{{ route('about-us') }}">About Us</a></li> -->
                                             <li
-                                                class="@if(Request::path()=='product-grids'||Request::path()=='product-lists')  active  @endif">
-                                                <a href="{{route('product-grids')}}">Products</a><span
-                                                    class="new">New</span>
+                                                class="@if (Request::path() == 'product-grids' || Request::path() == 'product-lists') active @endif">
+                                                <a href="{{ route('product-grids') }}">Produtos</a><span
+                                                    class="new">Novos</span>
                                             </li>
-                                            {{Helper::getHeaderCategory()}}
-                                            <!-- <li class="{{Request::path()=='blog' ? 'active' : ''}}"><a
-                                                    href="{{route('blog')}}">Blog</a></li> -->
+                                            {{ Helper::getHeaderCategory() }}
+                                            <!-- <li class="{{ Request::path() == 'blog' ? 'active' : '' }}"><a
+                                                    href="{{ route('blog') }}">Blog</a></li> -->
 
-                                            <li class="{{Request::path()=='contact' ? 'active' : ''}}"><a
-                                                    href="{{route('contact')}}">Contact Us</a></li>
+                                            <!-- <li class="{{ Request::path() == 'contact' ? 'active' : '' }}"><a
+                                                    href="{{ route('contact') }}">Contact Us</a></li> -->
                                         </ul>
                                     </div>
                                 </div>
